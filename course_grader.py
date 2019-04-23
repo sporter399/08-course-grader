324aae3aab92de96658f8bc7754ed5dc3639fabc
Steve Porter <sjporter399@gmail.com>
2019-04-22 21:07:58 -0600

    Update course_grader.py

--- course_grader.py
+++ course_grader.py
@@ -1,10 +1,10 @@
 def course_grader(test_scores):
     average_score = sum(test_scores)/len(test_scores)
     if all(x >= 50 and average_score >= 70 for x in test_scores):
-        return "Pass"
+        return "pass"
 
     else:
-        return "Fail"
+        return "fail"
 
 
 def main():
